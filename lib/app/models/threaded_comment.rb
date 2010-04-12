b0cb1e67c60a9c916925983b80a8d1cf22ae8c2d
Aaron Gough <aaron@aarongough.com>
2010-04-12 02:24:44 +0000

    ThreadedComment model setup and working

--- lib/app/models/threaded_comment.rb
+++ lib/app/models/threaded_comment.rb
@@ -1,5 +1,29 @@
 class ThreadedComment < ActiveRecord::Base
 
+  validates_presence_of   :threaded_comment_polymorphic_id, :threaded_comment_polymorphic_type, :parent_id
+  validates_length_of     :name, :within => 2..18
+  validates_length_of     :body, :within => 30..2000
+  validates_text_content  :body if( ActiveRecord::Base.respond_to?('validates_text_content'))
+  validates_length_of     :email, :minimum => 6
+  validates_format_of     :email, :with => /.*@.*\./
+
   belongs_to :threaded_comment_polymorphic, :polymorphic => true
+  
+  before_validation   :assign_owner_info_to_nested_comment
+  
+  attr_accessible :name, :body, :email, :parent_id, :threaded_comment_polymorphic_id, :threaded_comment_polymorphic_type
+  
+  def assign_owner_info_to_nested_comment
+    unless( self[:parent_id].nil? || self[:parent_id] == 0 )
+      parentComment = ThreadedComment.find(self[:parent_id])
+      self[:threaded_comment_polymorphic_id] = parentComment.threaded_comment_polymorphic_id
+      self[:threaded_comment_polymorphic_type] = parentComment.threaded_comment_polymorphic_type
+    end
+    self[:parent_id] = 0 if( self[:parent_id].nil? )
+  end
+  
+  def email_hash
+    return "#{self.email}-#{self.created_at}".hash.to_s(16)
+  end
 
 end
--- test/threaded_comment_test.rb
+++ test/threaded_comment_test.rb
@@ -17,80 +17,36 @@ class ThreadedCommentTest < Test::Unit::TestCase
     assert_kind_of ThreadedComment, ThreadedComment.new
   end 
   
-  def test_comment_should_be_created_with_period_as_punctuation
+  def test_comment_should_be_created
     @expected_comment_count = ThreadedComment.count + 1
     assert ThreadedComment.new(@sample_comment).save, "Could not save comment"
     assert_equal @expected_comment_count, ThreadedComment.count
   end
-  
-  def test_comment_should_be_created_with_question_mark_as_punctuation
-    @expected_comment_count = ThreadedComment.count + 1
-    assert ThreadedComment.new(@sample_comment.merge({:body => 'Is this a suitable sample comment?'})).save, "Could not save comment"
-    assert_equal @expected_comment_count, ThreadedComment.count
-  end
-  
-  def test_comment_should_be_created_with_exclamation_mark_as_punctuation
-    @expected_comment_count = ThreadedComment.count + 1
-    assert ThreadedComment.new(@sample_comment.merge({:body => 'This is a medium sized sample comment!' })).save, "Could not save comment"
-    assert_equal @expected_comment_count, ThreadedComment.count
-  end
-  
+    
   def test_comment_should_not_be_created_without_name
     @expected_comment_count = ThreadedComment.count
-    assert_nil ThreadedComment.new(@sample_comment.merge({:name => nil})).save
+    assert_equal false, ThreadedComment.new(@sample_comment.merge({:name => nil})).save
     assert_equal @expected_comment_count, ThreadedComment.count
   end
   
   def test_comment_should_not_be_created_without_body
     @expected_comment_count = ThreadedComment.count
-    assert_nil ThreadedComment.new(@sample_comment.merge({:body => nil})).save
+    assert_equal false, ThreadedComment.new(@sample_comment.merge({:body => nil})).save
     assert_equal @expected_comment_count, ThreadedComment.count
   end
   
   def test_comment_should_not_be_created_without_email
     @expected_comment_count = ThreadedComment.count
-    assert_nil ThreadedComment.new(@sample_comment.merge({:email => nil})).save
+    assert_equal false, ThreadedComment.new(@sample_comment.merge({:email => nil})).save
     assert_equal @expected_comment_count, ThreadedComment.count
   end
   
   def test_comment_should_not_be_created_with_junk_email
     @expected_comment_count = ThreadedComment.count
-    assert_nil ThreadedComment.new(@sample_comment.merge({:email => "asasdasdas"})).save
+    assert_equal false, ThreadedComment.new(@sample_comment.merge({:email => "asasdasdas"})).save
     assert_equal @expected_comment_count, ThreadedComment.count
   end
-  
-  def test_comments_with_junk_bodies_should_not_be_created
-    junk_comments = []
-    junk_comments << @sample_comment.merge({ :name => 'Test Commenter', :body => 'this comment has absolutely no capitalization.' })
-    junk_comments << @sample_comment.merge({ :name => 'Test Commenter', :body => 'This comment has no type of punctuation' })
-    junk_comments << @sample_comment.merge({ :name => "HORRIBLE", :body => "Fdkjd dk dkkjhd kjdhkjd kjdhdhdl alkasla lka alk." })
-    junk_comments << @sample_comment.merge({ :name => "This is junk", :body => "THIS COMMENT IS ALL IN UPPERCASE. ME NO WANT." })
-    junk_comments << @sample_comment.merge({ :name => "HORRIBLE", :body => "Thiscommenthasabsolutelynospacesthereisnowaythisisgood." })
-    junk_comments << @sample_comment.merge({ :name => "WORSE", :body => "THIS COMMENT is MORE THAN 80% CAPITALS." })
-    junk_comments << @sample_comment.merge({ :name => "BAD", :body => "THIS pERSON hAS tHEIR cAPS-lOCK oN." })
-    junk_comments.each do |junk_comment|
-      @expected_comment_count = ThreadedComment.count
-      assert_nil ThreadedComment.new( junk_comment ).save, "Comment rejection failed on: #{junk_comment.to_a.join(" : ")}"
-      assert_equal @expected_comment_count, ThreadedComment.count
-    end
-  end
-  
-  def test_comments_with_good_content_should_be_created
-    good_comments = []
-    good_comments << @sample_comment.merge({ :name => 'Test Commenter', :body => 'This is a very simple test comment.' })
-    good_comments << @sample_comment.merge({ :name => 'Gah!', :body => 'Supercalifragilisticexpialadocious: a super long word!' })
-    good_comments << @sample_comment.merge({ :name => 'Blah Comment', :body => 'How many words can I think of that don\'t contain an "e"?' })
-    good_comments << @sample_comment.merge({ :name => 'Captain Comment', :body => 'WHAT?! You mean to tell *ME* that SONY/ERICSSON *CAN\'T* make a phone?' })
-    good_comments << @sample_comment.merge({ :name => 'Captain Comment2', :body => 'WHAT?! YOU MEAN TO TELL ME that SONY/ERICSSON *CAN\'T* make a phone?' })
-    good_comments << @sample_comment.merge({ :name => 'Lolburger', :body => 'ROFL! Who do I have to shag to get a comment around here?' })
-    good_comments << @sample_comment.merge({ :name => 'Just \'Cause', :body => 'I\'m a Klingon. It\'s kind of my thing to be angry for almost no good reason. Yesterday I tore the door off my fridge cause I was out of margarine. These things happen.' })
-    good_comments.each do |good_comment|
-      @expected_comment_count = ThreadedComment.count + 1
-      assert ThreadedComment.new( good_comment ).save, "Comment creation failed on: #{good_comment.to_a.join(" : ")}"
-      assert_equal @expected_comment_count, ThreadedComment.count
-    end
-  end
-  
+    
   def test_nested_comment
     @test_comment = ThreadedComment.new(@sample_comment)
     assert @test_comment.save
