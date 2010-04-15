5d113d97ae3d722dfcfe4734286e70c1853c4237
Aaron Gough <aaron@aarongough.com>
2010-04-15 16:07:33 +0000

    Added tests for ThreadedCommentsController and wrote basic controller code

--- lib/app/controllers/threaded_comments_controller.rb
+++ lib/app/controllers/threaded_comments_controller.rb
@@ -1,3 +1,82 @@
 class ThreadedCommentsController < ActionController::Base
 
+  # GET /threaded-comments
+  def new
+    @comment = ThreadedComment.new(params[:threaded_comment])
+    @comment.name = session[:name] unless( session[:name].nil? )
+    @comment.email = session[:email] unless( session[:email].nil? )
+    render :layout => false
+  end
+
+  # POST /threaded-comments
+  def create
+    head :status => :failure and return if check_honeypot( 'comment' )
+    if( params[:threaded_comment][:parent_id] != 0 && !Comment.exists?(params[:threaded_comment][:parent_id]))
+      flash[:notice] = "The comment you were trying to comment on no longer exists."
+      head :status => :failure and return
+    end
+    @comment = ThreadedComment.new(params[:threaded_comment])
+    if( @comment.save )
+      session[:name] = @comment.name
+      session[:email] = @comment.email
+      render :action => 'show', :layout => false
+    else
+      render :action => 'new', :layout => false, :status => :failure
+    end
+  end
+  
+  # POST /threaded-comments/1/upmod
+  def upmod
+    if( ThreadedComment.exists?(params[:id]) && @comment = ThreadedComment.find(params[:id]) && @comment.increment!('rating'))
+      render :text => @comment.rating.to_s and return
+    end
+    head :bad_request
+  end
+  
+  # POST /threaded-comments/1/downmod
+  def downmod
+    if( ThreadedComment.exists?(params[:id]) && @comment = ThreadedComment.find(params[:id]) && @comment.decrement!('rating'))
+      render :text => @comment.rating.to_s and return
+    end
+    head :bad_request
+  end
+  
+  # POST /threaded-comments/1/flag
+  def flag
+    if( ThreadedComment.exists?(params[:id]) && @comment = ThreadedComment.find(params[:id]) && @comment.increment!('flags'))
+      render :text => "Thanks!" and return
+    end
+    head :bad_request
+  end
+  
+  # GET /threaded-comments/1/remove-notifications
+  def remove_notifications
+    @message = "The comment you are looking for has been removed or is incorrect." and render :action => 'remove_email' and return unless( ThreadedComment.exists?(params[:id]))
+    @comment = ThreadedComment.find(params[:id])
+    @message = "The information you provided does not match this comment." and render :action => 'remove_email' and return unless( params[:hash] == "#{@comment.email}-#{@comment.created_at}".hash.to_s(16) )
+    @message = "Thank-you. Your email (#{@comment.email}) has been removed."
+    @comment.notifications = false
+    @comment.save
+    render :action => 'remove_email'
+  end
+  
+  private
+  
+    def action_already_performed?
+      if( !session["/threaded-comments/#{params[:id]}/#{params[:action]}"].nil? )
+        return true
+      else
+        session["/threaded-comments/#{params[:id]}/#{params[:action]}"] = true
+        return false
+      end
+    end
+    
+    def check_honeypot( form_name, honeypot = "confirm_email" )
+      unless( params[form_name][honeypot].nil? || (params[form_name][honeypot].length == 0) )
+        redirect_to "/" and return true
+      end
+      params[form_name].delete( honeypot )
+      return false
+    end
+
 end
--- test/threaded_comments_controller_test.rb
+++ test/threaded_comments_controller_test.rb
@@ -15,11 +15,11 @@ class ThreadedCommentsControllerTest < ActionController::TestCase
     @response = ActionController::TestResponse.new 
     
     ActionController::Routing::Routes.draw do |map| 
-      map.flag_threaded_comment          '/threaded_comments/:id/flag', :controller => 'threaded_comments', :action => 'flag', :conditions => { :method => :post }
-      map.upmod_threaded_comment         '/threaded_comments/:id/upmod', :controller => 'threaded_comments', :action => 'upmod', :conditions => { :method => :post }
-      map.downmod_threaded_comment       '/threaded_comments/:id/downmod', :controller => 'threaded_comments', :action => 'downmod', :conditions => { :method => :post }
-      map.create_threaded_comment        '/threaded_comments', :controller => 'threaded_comments', :action => 'create', :conditions => { :method => :post }
-      map.remove_threaded_comment_email  '/threaded_comments/:id/remove-email/:hash', :controller => 'threaded_comments', :action => 'remove_email' 
+      map.flag_threaded_comment          '/threaded-comments/:id/flag', :controller => 'threaded_comments', :action => 'flag', :conditions => { :method => :post }
+      map.upmod_threaded_comment         '/threaded-comments/:id/upmod', :controller => 'threaded_comments', :action => 'upmod', :conditions => { :method => :post }
+      map.downmod_threaded_comment       '/threaded-comments/:id/downmod', :controller => 'threaded_comments', :action => 'downmod', :conditions => { :method => :post }
+      map.create_threaded_comment        '/threaded-comments', :controller => 'threaded_comments', :action => 'create', :conditions => { :method => :post }
+      map.remove_threaded_comment_email  '/threaded-comments/:id/remove-email/:hash', :controller => 'threaded_comments', :action => 'remove_email' 
     end  
     
     @sample_comment = {
@@ -36,11 +36,29 @@ class ThreadedCommentsControllerTest < ActionController::TestCase
     assert_equal @expected_comment_count, ThreadedComment.count
   end
   
+  def test_should_not_create_comment_if_negative_captcha_is_filled
+    assert_no_difference('ThreadedComment.count') do
+      put :create, :threaded_comment => @sample_comment.merge({:confirm_email => "test@example.com"})
+    end
+    assert_response :failure
+  end
+  
+  def test_new
+    session[:name] = "Test Name"
+    session[:email] = "Test Name"
+    get :new, :threaded_comment => @sample_comment.merge({:name => nil, :email => nil})
+    assert_response :success
+    assert_not_nil assigns(:comment)
+    assert @response.body.index(session[:name]), "Response body did not include commenter name"
+    assert @response.body.index(session[:email]), "Response body did not include commenter email"
+  end
+  
   def test_comment_upmod
     @expected_rating = ThreadedComment.find(1).rating + 1
     post :upmod, :id => 1
     assert_response :success
     assert_equal @expected_rating, ThreadedComment.find(1).rating
+    assert @response.body.index(@expected_rating.to_s), "Response body did not include new rating"
   end
   
   def test_comment_downmod
@@ -48,6 +66,7 @@ class ThreadedCommentsControllerTest < ActionController::TestCase
     post :downmod, :id => 1
     assert_response :success
     assert_equal @expected_rating, ThreadedComment.find(1).rating
+    assert @response.body.index(@expected_rating.to_s), "Response body did not include new rating"
   end
   
   def test_comment_flag
@@ -64,11 +83,11 @@ class ThreadedCommentsControllerTest < ActionController::TestCase
       { :action => 'downmod', :field => 'rating', :difference => -1}
     ]
     @actions.each do |action|
-      assert_difference("Comment.find(1).#{action[:field]}",action[:difference], "Action failed first time: #{action[:action]}") do
+      assert_difference("ThreadedComment.find(1).#{action[:field]}", action[:difference], "Action failed first time: #{action[:action]}") do
         put action[:action], :id => 1
         assert_redirected_to rant_path(Comment.find(1).rant)
       end 
-      assert_no_difference( "Comment.find(1).#{action[:field]}", "Action succeeded when it should have failed: #{action[:action]}") do
+      assert_no_difference( "ThreadedComment.find(1).#{action[:field]}", "Action succeeded when it should have failed: #{action[:action]}") do
         session[:last_url] = rant_path(Comment.find(1).rant)
         put action[:action], :id => 1
         assert_response :success
@@ -76,11 +95,11 @@ class ThreadedCommentsControllerTest < ActionController::TestCase
     end
   end
   
-  def test_should_remove_email_address_if_hash_matches
+  def test_should_remove_email_notifications_if_hash_matches
     test_comment = ThreadedComment.find(1)
     assert !test_comment.email.empty?
     assert test_comment.notifications == true
-    get :remove_email, :id => 1, :hash => test_comment.email_hash
+    get :remove_notifications, :id => 1, :hash => test_comment.email_hash
     assert_response :success
     test_comment.reload
     assert !test_comment.email.empty?
@@ -88,11 +107,11 @@ class ThreadedCommentsControllerTest < ActionController::TestCase
     assert @response.body.index( "removed" ), "Removal notice was not included in response body"
   end
   
-  def test_should_not_remove_email_address_if_hash_does_not_match
+  def test_should_not_remove_email_notifications_if_hash_does_not_match
     test_comment = ThreadedComment.find(1)
     assert !test_comment.email.empty?
     assert test_comment.notifications == true
-    get :remove_email, :id => 1, :hash => test_comment.email_hash + "1"
+    get :remove_notifications, :id => 1, :hash => test_comment.email_hash + "1"
     assert_response :success
     test_comment.reload
     assert !test_comment.email.empty?
