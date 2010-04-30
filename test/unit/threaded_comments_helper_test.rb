418595a6a1cbff4de301bacab7db04b7de96e040
Aaron Gough <aaron@aarongough.com>
2010-04-30 13:26:10 -0400

    Added failing test cases that expect THREADED_COMMENTS_CONFIG to be 'namespaced'. Added helper method for testing render_threaded_comments options

--- test/unit/threaded_comments_helper_test.rb
+++ test/unit/threaded_comments_helper_test.rb
@@ -28,84 +28,18 @@ class ThreadedCommentsHelperTest < ActionView::TestCase
     end
   end
   
-  test "render_threaded_comments should output comment rating & buttons by default" do
-    @test_comments.each do |comment|
-      assert @rendered_html.include?(comment.rating.to_s), "Did not include comment rating"
-      assert @rendered_html.include?(link_to_remote('',:url => {:controller => 'threaded_comments', :action => 'upmod', :id => comment.id})), "Did not include comment upmod button"
-      assert @rendered_html.include?(link_to_remote('',:url => {:controller => 'threaded_comments', :action => 'downmod', :id => comment.id})), "Did not include comment downmod button"
-    end
-  end
-  
-  test "render_threaded_comments should output comment rating & buttons if 'rating' is set in option, even if disabled in config" do
-    change_config_option("rating", false) do
-      @rendered_html = render_threaded_comments(@test_comments, :rating => true)
-      @test_comments.each do |comment|
-        assert @rendered_html.include?(comment.rating.to_s), "Did not include comment rating"
-        assert @rendered_html.include?(link_to_remote('',:url => {:controller => 'threaded_comments', :action => 'upmod', :id => comment.id})), "Did not include comment upmod button"
-        assert @rendered_html.include?(link_to_remote('',:url => {:controller => 'threaded_comments', :action => 'downmod', :id => comment.id})), "Did not include comment downmod button"
-      end
-    end
-  end
-  
-  test "render_threaded_comments should not output comment ratings & buttons if 'rating' disabled in options" do
-    @rendered_html = render_threaded_comments(@test_comments, :rating => false)
-    @test_comments.each do |comment|
-      assert !@rendered_html.include?(link_to_remote('',:url => {:controller => 'threaded_comments', :action => 'downmod', :id => comment.id})), "Should not include comment downmod button"
-      assert !@rendered_html.include?(link_to_remote('',:url => {:controller => 'threaded_comments', :action => 'upmod', :id => comment.id})), "Should not include comment upmod button"
-      assert !@rendered_html.include?("threaded_comment_rating_#{comment.id}"), "Should not include comment rating"
-    end
+  test "render_threaded_comment options and config" do
+    test_option "rating text", "enable_rating", "threaded_comment_rating_:id"
+    test_option "upmod button", "enable_rating", link_to_remote('', :url => {:controller => "threaded_comments", :action => "upmod", :id => ":id"})
+    test_option "downmod button", "enable_rating", link_to_remote('', :url => {:controller => "threaded_comments", :action => "downmod", :id => ":id"})
+    test_option "flag button", "enable_flagging", link_to_remote('', :url => {:controller => "threaded_comments", :action => "flag", :id => ":id"})
+    test_option "flag button container", "enable_flagging", "flag_threaded_comment_container_:id"
+    test_option "flag message", "flag_message", "Are you really sure you want to flag this comment?"
+    test_option "permalinks", "enable_permalinks", "#threaded_comment_:id"
+    test_option "reply link text", "reply_link_text", "Reply"
   end
   
   
-  test "render_threaded_comments should not output comment ratings & buttons if 'rating' disabled in config" do
-    change_config_option("rating", false) do
-      @test_comments.each do |comment|
-        assert !@rendered_html.include?(link_to_remote('',:url => {:controller => 'threaded_comments', :action => 'downmod', :id => comment.id})), "Should not include comment downmod button"
-        assert !@rendered_html.include?(link_to_remote('',:url => {:controller => 'threaded_comments', :action => 'upmod', :id => comment.id})), "Should not include comment upmod button"
-        assert !@rendered_html.include?("threaded_comment_rating_#{comment.id}"), "Should not include comment rating"
-      end
-    end
-  end
-  
-  test "render_threaded_comments should output comment flag button by default" do
-    @test_comments.each do |comment|
-      assert @rendered_html.include?(link_to_remote('flag',:url => {:controller => 'threaded_comments', :action => 'flag', :id => comment.id})), "Did not include comment flag button"
-    end
-  end
-  
-  test "render_threaded_comments should output comment flag button if 'flagging' set in options, even if disabled in config" do
-    change_config_option("flagging", false) do
-      @rendered_html = render_threaded_comments(@test_comments, :flagging => true)
-      @test_comments.each do |comment|
-        assert @rendered_html.include?(link_to_remote('flag',:url => {:controller => 'threaded_comments', :action => 'flag', :id => comment.id})), "Did not include comment flag button"
-      end
-    end
-  end
-  
-  test "render_threaded_comments should not output comment flag button if 'flagging' disabled in options" do
-    @rendered_html = render_threaded_comments(@test_comments, :flagging => false)
-    @test_comments.each do |comment|
-      assert !@rendered_html.include?(link_to_remote('flag',:url => {:controller => 'threaded_comments', :action => 'flag', :id => comment.id})), "Should not include comment flag button"
-    end
-  end
-  
-  test "render_threaded_comments should not output comment flag button if 'flagging' disabled in config" do
-    change_config_option("flagging", false) do
-      @test_comments.each do |comment|
-        assert !@rendered_html.include?(link_to_remote('flag',:url => {:controller => 'threaded_comments', :action => 'flag', :id => comment.id})), "Should not include comment flag button"
-      end
-    end
-  end
-  
-  test "render_threaded_comments should output anchors for each comment" do
-    @test_comments.each do |comment|
-      assert @rendered_html.include?("#threaded_comment_#{comment.id}"), "Did not include comment anchor"
-    end
-  end
-  
-  test "render_threaded_comments should output reply link for each comment" do
-    assert_equal @test_comments.length, @rendered_html.split("/threaded_comments/new").length - 1
-  end
   
   # Stub out some of ActionView's helpers so we can test *our* helpers in isolation
   def link_to_remote(*args)
@@ -126,6 +60,59 @@ class ThreadedCommentsHelperTest < ActionView::TestCase
   
   private
   
+    def test_option(name, option_name, pattern, namespace = 'render_threaded_comments')
+      assert defined?(THREADED_COMMENTS_CONFIG[namespace][option_name]), "The option name '#{namespace}:#{option_name}' was not set in the default config"
+      if(THREADED_COMMENTS_CONFIG[namespace][option_name].is_a?(TrueClass) or THREADED_COMMENTS_CONFIG[namespace][option_name].is_a?(FalseClass))
+        # Enabled in config - not set in options
+        change_config_option(namespace, option_name, true) do
+          rendered_html = render_threaded_comments(@test_comments)
+          @test_comments.each do |comment|
+            assert rendered_html.include?(pattern.gsub(":id", comment.id.to_s)), "render_threaded_comments did not output '#{pattern.gsub(":id", comment.id.to_s)}' with '#{namespace}:#{option_name}' enabled in config"
+          end
+        end
+        # Disabled in config - not set in options
+        change_config_option(namespace, option_name, false) do
+          rendered_html = render_threaded_comments(@test_comments)
+          @test_comments.each do |comment|
+            assert !rendered_html.include?(pattern.gsub(":id", comment.id.to_s)), "render_threaded_comments should not output '#{pattern.gsub(":id", comment.id.to_s)}' when '#{namespace}:#{option_name}' disabled in config"
+          end
+        end
+        # Enabled in options - disabled in config - options should override
+        change_config_option(namespace, option_name, false) do
+          rendered_html = render_threaded_comments(@test_comments, option_name => true)
+          @test_comments.each do |comment|
+            assert rendered_html.include?(pattern.gsub(":id", comment.id.to_s)), "render_threaded_comments did not output '#{pattern.gsub(":id", comment.id.to_s)}' when '#{namespace}:#{option_name}' enabled in options"
+          end
+        end
+        # Disabled in options - enabled in config - options should override
+        change_config_option(namespace, option_name, true) do
+          rendered_html = render_threaded_comments(@test_comments, option_name => false)
+          @test_comments.each do |comment|
+            assert !rendered_html.include?(pattern.gsub(":id", comment.id.to_s)), "render_threaded_comments should not output '#{pattern.gsub(":id", comment.id.to_s)}' when '#{namespace}:#{option_name}' disabled in options"
+          end
+        end
+      elsif(THREADED_COMMENTS_CONFIG[namespace][option_name].is_a?(String))
+        # Default - should be set in config
+        rendered_html = render_threaded_comments(@test_comments)
+        assert_equal @test_comments.length, rendered_html.split(pattern).length - 1, "render_threaded_comments did not output '#{pattern}' for each comment by default"
+        # Set in config - not set in options
+        change_config_option(namespace, option_name, "replacement_pattern_config") do
+          rendered_html = render_threaded_comments(@test_comments)
+          assert_equal @test_comments.length, rendered_html.split("replacement_pattern_config").length - 1, "render_threaded_comments did not output value of '#{namespace}:#{option_name}' for each comment when set in config"
+          assert_equal 1, rendered_html.split(pattern), "render_threaded_comments still output default value of '#{namespace}:#{option_name}' even when overwritten in config"
+        end
+        # Set in options - also set in config - options should override
+        change_config_option(namespace, option_name, "replacement_pattern_config") do
+          rendered_html = render_threaded_comments(@test_comments, option_name => "replacement_pattern_options")
+          assert_equal @test_comments.length, rendered_html.split("replacement_pattern_options").length - 1, "render_threaded_comments did not output value of '#{namespace}:#{option_name}' for each comment when set in options"
+          assert_equal 1, rendered_html.split(pattern), "render_threaded_comments still output default value of '#{namespace}:#{option_name}' even when overwritten in config and options"
+          assert_equal 1, rendered_html.split("replacement_pattern_config"), "render_threaded_comments still output config value of '#{namespace}:#{option_name}' even when overwritten in options"
+        end
+      else
+        flunk "Unrecognized option type: #{THREADED_COMMENTS_CONFIG[namespace][option_name].class}"
+      end
+    end
+  
     def complex_thread(length=100)
       comments = []
       length.times do
@@ -143,13 +130,12 @@ class ThreadedCommentsHelperTest < ActionView::TestCase
       comments
     end
     
-    def change_config_option(key, value, &block)
+    def change_config_option(namespace, key, value, &block)
       old_config = THREADED_COMMENTS_CONFIG.dup
       old_stderr = $stderr
       $stderr = StringIO.new
-      THREADED_COMMENTS_CONFIG[key] = value
+      THREADED_COMMENTS_CONFIG[namespace][key] = value
       $stderr = old_stderr
-      @rendered_html = render_threaded_comments(@test_comments)
       yield block
     ensure
       $stderr = StringIO.new
