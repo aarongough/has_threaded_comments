20bcc4a0eb9d6935eb3db50c10a0d835b1aca232
Aaron Gough <aaron@aarongough.com>
2010-05-05 12:36:12 -0400

    Altered laoding of threaded_comments_config.yml so that only one environment needs to be defined. Other environments will default to using the production settings unless explicitly setup.

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,6 +1,11 @@
-load_path = "#{RAILS_ROOT}/config/threaded_comments_config.yml"
+load_path = File.expand_path("#{RAILS_ROOT}/config/threaded_comments_config.yml")
 if( File.exists?(load_path))
-  temp = YAML.load_file(File.join(File.dirname(__FILE__), "..", "..", "generators", "install_has_threaded_comments", "templates", "threaded_comments_config.yml"))[RAILS_ENV]
+  temp = YAML.load_file(load_path)
+  if(!temp[RAILS_ENV].nil?)
+    temp = temp[RAILS_ENV]
+  else
+    temp = temp['production']
+  end
   THREADED_COMMENTS_CONFIG = {}
   temp.each_pair do |key, value|
     THREADED_COMMENTS_CONFIG[key.to_sym] = value.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
