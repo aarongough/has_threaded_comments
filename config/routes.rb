e260f80540b57824030c4ed6b03c98719e25c006
Aaron Gough <aaron@aarongough.com>
2010-05-05 18:58:29 -0400

    Normalized routes to use path '/threaded-comments' [issue 3]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resource :threaded_comment, :except => [:index, :edit, :delete]
+  map.resource :threaded_comment, :except => [:index, :edit, :delete], :as => 'threaded-comments'
   map.flag_threaded_comment                   '/threaded-comments/:id/flag', :controller => 'threaded_comments', :action => 'flag', :conditions => { :method => :post }
   map.upmod_threaded_comment                  '/threaded-comments/:id/upmod', :controller => 'threaded_comments', :action => 'upmod', :conditions => { :method => :post }
   map.downmod_threaded_comment                '/threaded-comments/:id/downmod', :controller => 'threaded_comments', :action => 'downmod', :conditions => { :method => :post }
