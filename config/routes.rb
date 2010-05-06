9540ffe2bf5b75859834b4c70977b7ccf34a7715
Aaron Gough <aaron@aarongough.com>
2010-05-06 00:38:13 -0400

    Switched to manual route declaration. Rails was getting confused when using map.resources in conjunction with the custom actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :threaded_comment, :only => [:new, :show, :create], :as => 'threaded-comments'
+  map.create_threaded_comment                 '/threaded-comments', :controller => 'threaded_comments', :action => 'create', :conditions => { :method => :post }
+  map.new_threaded_comment                    '/threaded-comments/new', :controller => 'threaded_comments', :action => 'new', :conditions => { :method => :get }
+  map.threaded_comment                        '/threaded-comments/:id', :controller => 'threaded_comments', :action => 'show', :conditions => { :method => :get }
   map.flag_threaded_comment                   '/threaded-comments/:id/flag', :controller => 'threaded_comments', :action => 'flag', :conditions => { :method => :post }
   map.upmod_threaded_comment                  '/threaded-comments/:id/upmod', :controller => 'threaded_comments', :action => 'upmod', :conditions => { :method => :post }
   map.downmod_threaded_comment                '/threaded-comments/:id/downmod', :controller => 'threaded_comments', :action => 'downmod', :conditions => { :method => :post }
