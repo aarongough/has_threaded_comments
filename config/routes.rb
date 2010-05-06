91c6dea5013849a79f1a96d064b589630ee3284b
Aaron Gough <aaron@aarongough.com>
2010-05-05 23:56:31 -0400

    Updated routes. Routes were incorrectly being generated for several non-existant controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resource :threaded_comment, :except => [:index, :edit, :delete], :as => 'threaded-comments'
+  map.resources :threaded_comment, :only => [:new, :show, :create], :as => 'threaded-comments'
   map.flag_threaded_comment                   '/threaded-comments/:id/flag', :controller => 'threaded_comments', :action => 'flag', :conditions => { :method => :post }
   map.upmod_threaded_comment                  '/threaded-comments/:id/upmod', :controller => 'threaded_comments', :action => 'upmod', :conditions => { :method => :post }
   map.downmod_threaded_comment                '/threaded-comments/:id/downmod', :controller => 'threaded_comments', :action => 'downmod', :conditions => { :method => :post }
-  map.remove_threaded_comment_notifications   '/threaded-comments/:id/remove-notifications/:hash', :controller => 'threaded_comments', :action => 'remove_notifications' 
+  map.remove_threaded_comment_notifications   '/threaded-comments/:id/remove-notifications/:hash', :controller => 'threaded_comments', :action => 'remove_notifications', :conditions => { :method => :get }
 end
