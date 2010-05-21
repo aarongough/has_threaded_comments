cd21c21caebbc01448dc28fde7ca373556e76832
Aaron Gough <aaron@aarongough.com>
2010-05-21 01:21:37 -0400

    Added loading gif to generator template files, added test for checking generator, changed generator

--- generators/install_has_threaded_comments/install_has_threaded_comments_generator.rb
+++ generators/install_has_threaded_comments/install_has_threaded_comments_generator.rb
@@ -7,6 +7,7 @@ class InstallHasThreadedCommentsGenerator < Rails::Generator::Base
       m.directory "public/has-threaded-comments-images"
       m.file "downmod-arrow.gif", "public/has-threaded-comments-images/downmod-arrow.gif"
       m.file "upmod-arrow.gif", "public/has-threaded-comments-images/upmod-arrow.gif"
+      m.file "ajax-loader.gif", "public/has-threaded-comments-images/ajax-loader.gif"
       m.migration_template "create_threaded_comments.rb", "db/migrate"
     end
   end
